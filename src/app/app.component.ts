366f4d1a90a4439903d98f3a04c00e1d9dc7ec60
Alexjrf03 <alexjrf27@gmail.com>
2022-03-15 17:10:52 -0400

    pipedate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,36 @@ export class AppComponent {
 
   nombre:string = 'Capitan America';
   
+  arreglo:number[] = [1,2,3,4,5,6,7,8,9,10];
+
+  PI:number = Math.PI;
+
+  porcentaje:number = 0.234;
+
+  salario:number = 1234.5;
+
+  fecha:Date = new Date();
+
+  valorPromesa = new Promise( (resolve) => {
+
+    setTimeout(() => {
+      
+      resolve('llego la data');
+
+    }, 4500);
+
+  })
+
+  heroe = {
+
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'primera',
+      casa:20
+    }
+
+  }
+
 }
